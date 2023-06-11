c2d0572e71124a5456dac10580cd6d506118d441
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2023-06-11 20:04:14 +0530

    feature: basic navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Navbar.js/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Shoaib</h1>
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Components/Navbar.js/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <p>Socials</p>
+      <input className="navbar-search" placeholder="Search" />
+      <p>Profile</p>
+    </nav>
+  );
+}
