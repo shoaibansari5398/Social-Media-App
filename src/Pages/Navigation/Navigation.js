861f5ea7e7867ad9c754eec3cb34457e47a22e11
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2023-06-13 19:39:57 +0530

    UI: include material icon in navigation panel

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -1,12 +1,36 @@
 import "./Navigation.css";
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
 export default function Navigation() {
   return (
     <div className="navigation">
-      <h3>Home</h3>
-      <h3>Explore</h3>
-      <h3>Liked Post</h3>
-      <h3>Bookmarks</h3>
+      <p>
+        <span>
+          <HomeIcon />
+        </span>
+        Home
+      </p>
+      <p>
+        <span>
+          <ExploreIcon />
+        </span>
+        Explore
+      </p>
+      <p>
+        <span>
+          <BookmarkIcon />
+        </span>
+        Bookmarks
+      </p>
+      <p>
+        <span>
+          <FavoriteIcon />
+        </span>
+        Liked Post
+      </p>
       <button>Create Post</button>
     </div>
   );
