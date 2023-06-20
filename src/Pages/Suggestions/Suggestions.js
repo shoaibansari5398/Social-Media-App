c37522a298f8b9bf130b09fdfecea9a66f133e3b
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2023-06-20 08:48:48 +0530

    feat: set uo data context and improve UI

--- src/Pages/Suggestions/Suggestions.js
+++ src/Pages/Suggestions/Suggestions.js
@@ -1,19 +1,16 @@
 import "./Suggestions.css";
+import { Avatar } from "@mui/material";
 
 export default function Suggestions() {
   return (
     <div className="suggestions">
-      <div className="suggestions-latest">
-        <button>Trending</button>
-        <button>Latest</button>
-      </div>
       <h3>Suggestions for you</h3>
       <div className="user-suggestion">
-        <img src="" alt="pp" className="user-suggestion-img" />
+        <Avatar src="/static/images/avatar/1.jpg" />
         <div className="user-suggestion-name">
           <div>
-            <p>n</p>
-            <p>Id</p>
+            <p>Shoaib</p>
+            <p>@shoaibansari</p>
           </div>
           <button>Follow</button>
         </div>
